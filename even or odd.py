24664f4a9ee23c1877c78799b251eeb112d03139
janakijaanu <42801567+janakijaanu@users.noreply.github.com>
2018-09-25 09:25:24 +0530

    Update even or odd.py

--- even or odd.py
+++ even or odd.py
@@ -1,6 +1,6 @@
-num = int(input("choose the number: "))
+num = int(input(" "))
 mod = num % 2
 if mod > 0:
-    print("The number is odd.")
+    print("odd")
 else:
-    print("The number is even.")
+    print("even")
